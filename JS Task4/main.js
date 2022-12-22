956f7ceef73c3daf63c056577e29cb6f570a92bc
MohamedAlhammadi <117170768+MohamedAlhammadi@users.noreply.github.com>
2022-12-22 21:35:49 +0400

    Add files via upload

---
+++ JS Task4/main.js
@@ -0,0 +1,91 @@
+const addSingleProductToCart = (product) => {
+    const products = JSON.parse(localStorage.getItem("products") || "[]");
+    const oldProductIndex = products.findIndex((x) => x.id === product.id);
+    if (oldProductIndex >= 0) {
+      products[oldProductIndex].quantity += 1;
+    } else {
+      products.push({ ...product, quantity: 1 });
+      console.log({ product, quantity: 1 })
+    }
+    localStorage.setItem("products", JSON.stringify(products));
+  };
+  
+(function ($) {
+    "use strict";
+    
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    $('.vendor-carousel').owlCarousel({
+        loop: true,
+        margin: 29,
+        nav: false,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive: {
+            0:{
+                items:2
+            },
+            576:{
+                items:3
+            },
+            768:{
+                items:4
+            },
+            992:{
+                items:5
+            },
+            1200:{
+                items:6
+            }
+        }
+    });
+
+    $('.related-carousel').owlCarousel({
+        loop: true,
+        margin: 29,
+        nav: false,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            992:{
+                items:4
+            }
+        }
+    });
+    
+})(jQuery);
---
+++ JS Task4/script.js
@@ -0,0 +1,115 @@
+class OrderProduct {
+        id;
+        productName;
+        price;
+        quantity;
+        image;
+        constructor(product) {
+          this.id=product.id;
+          this.productName = product.productName;
+          this.price = product.price;
+          this.quantity = product.quantity;
+          this.image=product.productName;
+        }
+        incQuantity = ()=>{this.quantity++;}
+        decQuantity = ()=>{if(this.quantity>1)this.quantity--;}
+      }
+      
+      class Cart {
+        products = [];
+        constructor() {
+          this.products=[];
+        }
+        get shipping (){
+          return this.products.length * 10;
+        };
+      
+        get subTotal (){
+          if (this.products.length > 0) {
+            return this.products.map((p) => p.price * p.quantity).reduce((a, e) => (a += e));
+          } else {return 0;}};
+      
+        get total () {
+          this.shipping + this.subTotal;}
+      
+        decQuantity = (i) => {
+          this.products[i].decQuantity();
+          this.saveChanges();
+          this.setproducts();
+        };
+        incQuantity = (i) => {
+          this.products[i].incQuantity();
+          this.saveChanges();
+          this.setproducts();
+        };
+      
+        remove = (i) => {
+          this.products.splice(i, 1);
+          this.saveChanges();
+          this.setproducts();
+        };
+        addProduct(product) {
+          let orderProduct = new OrderProduct(product);
+          this.products.push(orderProduct);
+        }
+        
+        saveChanges() {
+          const products = [];
+          this.products.forEach((d) => {
+              products.push(d);
+          });
+          localStorage.setItem("products", JSON.stringify(products));
+        }
+        setproducts = () => {
+          console.log(this.products)
+         document.getElementById("products").innerHTML = "";
+         if (this.products.length > 0) {
+           this.products.forEach((p, i) => {
+             document.getElementById("products").innerHTML += setHTMLRow(p, i);
+           });
+         }
+         document.getElementById("shipping").innerHTML = `$${this.shipping}`;
+         document.getElementById("sub-total").innerHTML = `$${this.subTotal}`;
+         document.getElementById("total").innerHTML = `$${this.shipping+this.subTotal}`;
+         console.log(this.total)
+        };
+      }
+      
+      let productTable = document.getElementById("products");
+      
+      
+      const setHTMLRow = (p, i) => {
+        return `
+        <tr>
+        <td class="align-middle"><img src="img/${
+          p.productName
+        }.jpg" alt="" style="width: 50px;"> ${p.productName}</td>
+        <td class="align-middle">$${p.price}</td>
+        <td class="align-middle">
+        <div class="input-group quantity mx-auto" style="width: 100px;">
+        <div class="input-group-btn">
+        <button type="button" class="btn btn-sm btn-primary btn-minus" onclick="cart.decQuantity(${i});">
+        <i class="fa fa-minus"></i>
+        </button>
+        </div>
+        <input type="text" class="form-control form-control-sm bg-secondary border-0 text-center" value="${
+          p.quantity
+        }">
+        <div class="input-group-btn">
+        <button type="button" class="btn btn-sm btn-primary btn-plus" onclick="cart.incQuantity(${i});">
+        <i class="fa fa-plus"></i>
+        </button>
+        </div>
+        </div>
+        </td>
+        <td class="align-middle">$${p.price * p.quantity}</td>
+        <td class="align-middle"><button class="btn btn-sm btn-danger" type="button" onclick="cart.remove(${i});cart.saveChanges();"><i class="fa fa-times"></i></button></td>
+        </tr>`;
+      };
+      
+      let cart = new Cart([]);
+      const products = JSON.parse(localStorage.getItem("products") ?? "[]");
+      products.forEach((x) => {
+        cart.addProduct(x);
+      });
+      cart.setproducts();
